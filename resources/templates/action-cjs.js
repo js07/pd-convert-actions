db7bd5e40894033b3408f6c37a87ccdd389b06ea
js07 <19861096+js07@users.noreply.github.com>
2023-05-08 13:25:46 -0400

    fix indentation of generated component code

--- resources/templates/action-cjs.js
+++ resources/templates/action-cjs.js
@@ -13,55 +13,61 @@ module.exports = {
 
 {{~#*inline "componentProperties"}}
 {{#if key}}
-  key: {{tostring key}},
+key: {{tostring key}},
 {{/if}}
 {{#if name}}
-  name: {{tostring name}},
+name: {{tostring name}},
 {{/if}}
 {{#if description}}
-  description: {{tostring description}},
+description: {{tostring description}},
 {{/if}}
 {{#if version}}
-  version: {{tostring version}},
+version: {{tostring version}},
 {{/if}}
 {{#if key}}
-  type: "action",
+type: "action",
 {{/if}}
 {{#if props.[0]}}
-  props: {
-  {{#each props}}
-    {{> componentProp }}
-  {{/each}}
-  },
+props: {
+{{#each props}}
+  {{> componentProp }}
+{{/each}}
+},
 {{/if}}
-  async run({ steps, $ }) {{{code}}}
+async run({ steps, $ }) {
+  {{> runCode}}
+}
+{{/inline}}
+
+{{~#*inline "runCode"}}
+{{{code}}}
 {{/inline}}
 
 {{~#*inline "componentProp"}}
-  {{#if this.isString}}  
-    {{this.key}}: "{{this.type}}",
-  {{else}}
-    {{this.key}}: {
-      type: "{{this.type}}",
-    {{#if this.app}}
-      app: {{tostring this.app}},
-    {{/if}}
-    {{#if (isdefined this.label)}}
-      label: {{{tostring this.label}}},
-    {{/if}}
-    {{#if (isdefined this.description)}}
-      description: {{tostring this.description}},
-    {{/if}}
-    {{#if (isdefined this.optional)}}
-      optional: {{this.optional}},
-    {{/if}}
-    {{#if (isdefined this.options)}}
-      options: [
-      {{#each this.options}}
-        {{tostring this}},
-      {{/each}}
-      ],
-    {{/if}}
-    },
+{{#if this.isString}}  
+{{this.key}}: "{{this.type}}",
+{{else}}
+{{this.key}}: {
+  type: "{{this.type}}",
+  {{#if this.app}}
+  app: {{tostring this.app}},
   {{/if}}
+  {{#if (isdefined this.label)}}
+  label: {{{tostring this.label}}},
+  {{/if}}
+  {{#if (isdefined this.description)}}
+  description: {{tostring this.description}},
+  {{/if}}
+  {{#if (isdefined this.optional)}}
+  optional: {{this.optional}},
+  {{/if}}
+  {{#if (isdefined this.options)}}
+  options: [
+  {{#each this.options}}
+    {{tostring this}},
+  {{/each}}
+  ],
+  {{/if}}
+},
+{{/if}}
 {{/inline}}`;
